cecbc94cd410f1eaca1f5126c1f5537ec8f4e206
Rafa Pereira <rp1437508@gmail.com>
2024-06-13 15:12:10 -0300

    adição de gifs, criação de novas pages, mudanças no banner principal e ajustes finos

--- js/setup.js
+++ js/setup.js
@@ -15,37 +15,34 @@ $('.owl-carousel').owlCarousel({
     }
 })
 
+var movieMainImage = document.querySelector("main")
 
-var tela_kof97 = document.getElementById('kof97')
-// var backgroud_kof2002 = document.getElementsByClassName('movie-main')
+if(document.getElementById("kof97")) {
+    movieMainImage.style.backgroundImage = 'linear-gradient(rgba(0,0,0,.30),rgba(0,0,0,.30)100%),url(../img/capa/Kof/kof97.jpg)';
+    document.body.style.backgroundSize = 'cover' 
 
-// if (tela_kof97) {
-//     document.body.style.background = 'linear-gradient(rgba(0,0,0,.60),rgba(0,0,0,.60)100%),url(../img/capa/kof97.jpg)'
-//     document.body.style.backgroundSize = 'cover'
-
-// } else {
-//     document.body.style.background = 'linear-gradient(rgba(0,0,0,.60),rgba(0,0,0,.60)100%),url(../img/capa/Capa2002.jpg)'
-//     document.body.style.backgroundSize = 'cover'
-// }
-
-// var movieMainImage = document.querySelector(".movie-main")
-// var movieMainImageID = document.getElementById("kof97")
-
-// if(movieMainImageID) {
-//     movieMainImage.style.backgroundImage = 'url(../img/capa/kof97.jpg)';
-
-// }else{
-//     movieMainImage.style.backgroundImage = 'url(../img/capa/Capa2002.jpg)';
-// }
+}else if(document.getElementById("kof2003")){
+    movieMainImage.style.backgroundImage = 'linear-gradient(rgba(0,0,0,.30),rgba(0,0,0,.30)100%),url(../img/capa/Kof/capakof2003.gif)';
+    document.body.style.backgroundSize = 'cover'
+}else if(document.getElementById("garou")){
+    movieMainImage.style.backgroundImage = 'linear-gradient(rgba(0,0,0,.30),rgba(0,0,0,.30)100%),url(../img/capa/Ffury/garou_capa.gif)';
+    document.body.style.backgroundSize = 'cover'
+}else if(document.getElementById("realB")){
+    movieMainImage.style.backgroundImage = 'linear-gradient(rgba(0,0,0,.30),rgba(0,0,0,.30)100%),url(../img/capa/Ffury/real_capa.gif)';
+    document.body.style.backgroundSize = 'cover'
+}else if(document.getElementById("lastB2")){
+    movieMainImage.style.backgroundImage = 'linear-gradient(rgba(0,0,0,.30),rgba(0,0,0,.30)100%),url(../img/capa/LastBlade/lastblade2.gif)';
+    document.body.style.backgroundSize = 'cover'
+}
 
 
-document.addEventListener("DOMContentLoaded", function() {
-    var carouselImages = document.querySelectorAll('.carousel-movie .item img');
-    carouselImages.forEach(function(image) {
-        image.addEventListener('click', function() {
-            // Altera a imagem de fundo do elemento com a classe movie-main
-            var movieMain = document.querySelector('.movie-main');
-            movieMain.style.backgroundImage = 'url(' + this.src + ')';
-        });
-    });
-});
+// document.addEventListener("DOMContentLoaded", function() {
+//     var carouselImages = document.querySelectorAll('.carousel-movie .item img');
+//     carouselImages.forEach(function(image) {
+//         image.addEventListener('click', function() {
+//             // Altera a imagem de fundo do elemento com a classe movie-main
+//             var movieMain = document.querySelector('.movie-main');
+//             movieMain.style.backgroundImage = 'url(' + this.src + ')';
+//         });
+//     });
+// });
